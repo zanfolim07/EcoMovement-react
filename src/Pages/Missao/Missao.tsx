0596991e1a82cc6ffccf82677944ed2bebba6dfd
monteiroo5 <cttlucasmonteirodc@gmail.com>
2026-09-12 16:20:48 -0300

    feat: add dados etapas de missoes

---
+++ src/Pages/Missao/Missao.tsx
@@ -0,0 +1,46 @@
+
+import cell1 from '../../assets/img/cell-1.png';
+import cell2 from '../../assets/img/cell-2.png';
+import cell3 from '../../assets/img/cell-3.png';
+import cell4 from '../../assets/img/cell-4.png';
+import cell5 from '../../assets/img/cell-5.png';
+import cell6 from '../../assets/img/cell-6.png';
+
+const passos = [
+  {
+    image: cell1,
+    title: 'Acesse a aba Missões na seção EcoMovement.',
+    description:
+      'Acesse a área de missões gerais do app e selecione EcoMovement para encontrar as missões de sustentabilidade disponíveis.',
+  },
+  {
+    image: cell2,
+    title: 'Leia as orientações da missão.',
+    description:
+      'Confira as instruções, o formato da validação e os critérios necessários antes de iniciar a atividade.',
+  },
+  {
+    image: cell3,
+    title: 'Envie a comprovação da evidência.',
+    description:
+      'Realize a atividade e envie a foto ou o vídeo solicitado para que a missão possa ser analisada.',
+  },
+  {
+    image: cell4,
+    title: 'Acompanhe o progresso da missão.',
+    description:
+      'Após o envio, acompanhe o andamento da validação e as informações sobre a pontuação da missão.',
+  },
+  {
+    image: cell5,
+    title: 'Confira as missões concluídas.',
+    description:
+      'Acesse o histórico para verificar as missões aprovadas ou rejeitadas e acompanhar sua evolução.',
+  },
+  {
+    image: cell6,
+    title: 'Respeite o tempo limite da atividade.',
+    description:
+      'Se a missão tiver tempo máximo, conclua e envie a evidência dentro do prazo indicado na tela.',
+  },
+];
